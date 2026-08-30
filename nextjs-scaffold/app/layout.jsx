2eb7cbf5801ee8ccad89185f743a785f500b32ac
matsero-11 <mafsful08@gmail.com>
2026-08-30 22:55:30 +0200

    Update layout.jsx

--- nextjs-scaffold/app/layout.jsx
+++ nextjs-scaffold/app/layout.jsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body>
-        <div className="max-w-md md:max-w-xl lg:max-w-3xl mx-auto w-full">
+        <div className="max-w-md md:max-w-2xl lg:max-w-4xl mx-auto w-full px-5 md:px-8 py-6 md:py-10 space-y-6 md:space-y-8">
           {children}
         </div>
         <RegisterServiceWorker />
@@ -27,3 +27,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
