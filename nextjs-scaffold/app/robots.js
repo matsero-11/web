c55f8007e76b685c205661c54a0d1f147605538d
matsero-11 <mafsful08@gmail.com>
2026-09-02 21:02:13 +0200

    Update robots.js

--- nextjs-scaffold/app/robots.js
+++ nextjs-scaffold/app/robots.js
@@ -4,8 +4,8 @@ export default function robots() {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
-      disallow: ["/api/", "/_next/"],
+      allow: ["/", "/_next/static/", "/_next/image/"],
+      disallow: ["/api/"],
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
     host: BASE_URL,
